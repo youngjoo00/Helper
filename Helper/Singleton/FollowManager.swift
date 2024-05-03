c28f203f1792e47f71d79086e0135f44725a235e
youngjoo00 <yungjoo0624@gmail.com>
2024-05-04 01:51:18 +0900

    update: 폴더링 변경, present 와 관련된 폴더로 병합

--- Helper/Singleton/FollowManager.swift
+++ Helper/Singleton/FollowManager.swift
@@ -12,7 +12,7 @@ final class FollowManager {
     static let shared = FollowManager()
     private init() {}
     
-    /// 내 팔로잉 목록과 다른 유저의 팔로우 목록을 비교한 결과를 Bool 배열로 반환합니다.
+    /// 내 팔로잉 목록과 다른 유저의 팔로우 목록을 비교한 결과를 Bool 배열로 반환
     func checkedFollowingList(_ myFollowingList: [UserResponse.Follow], otherFollowList: [UserResponse.Follow]) -> [Bool] {
         let myFollowingSet: Set<String> = Set(myFollowingList.map { $0.userID })
         print(myFollowingSet, otherFollowList)
